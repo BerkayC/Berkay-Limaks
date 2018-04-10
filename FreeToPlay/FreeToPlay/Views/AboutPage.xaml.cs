85c0cfa8b8f023f56bcea1dbedb8832991f287ef
Hasan Çelebi <hasancelebi@hasans-imac.mynet>
2018-04-10 10:39:08 +0300

    Basla
    Başladım

---
+++ FreeToPlay/FreeToPlay/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FreeToPlay
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
