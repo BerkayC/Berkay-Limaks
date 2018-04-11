8ff98522a2443fafcb87672a531204898e3036a1
Hasan Çelebi <hasancelebi@hasans-imac.mynet>
2018-04-11 17:40:55 +0300

    listviewManipulation

--- FreeToPlay/FreeToPlay/mViews/ListViewPage.xaml.cs
+++ FreeToPlay/FreeToPlay/mViews/ListViewPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Xamarin.Forms;
 
 namespace FreeToPlay.mViews
@@ -9,19 +9,64 @@ namespace FreeToPlay.mViews
     {
         public class MyObject
         {
-            public int Id{get;set;}
-            public string Name{get;set;}
+            public int Id {get;set;}
+            public String Name{get;set;}
         }
 
+        public List<MyObject> myObjects{ get; set; }
 
         public ListViewPage()
         {
             InitializeComponent();
-            List<MyObject> myObjects = new List<MyObject>();
-            myObjects.Add(new MyObject { Id = 1, Name="Kitap" });
-            myObjects.Add(new MyObject { Id = 2, Name = "Fare" });
-            myObjects.Add(new MyObject { Id = 3, Name = "Telefon" });
-            myObjects.Add(new MyObject { Id = 4, Name = "Cüzdan" });
+            myObjects = new List<MyObject>();
+
+            if (myObjects !=null)
+            {
+				myObjects.Add(new MyObject { Id = 1, Name="Kitap" });
+				myObjects.Add(new MyObject { Id = 2, Name = "Fare" });
+                myObjects.Add(new MyObject { Id = 5, Name = "Fare" });
+				myObjects.Add(new MyObject { Id = 3, Name = "Telefon" });
+				myObjects.Add(new MyObject { Id = 4, Name = "Cüzdan" });
+
+            }
+            lstView.ItemsSource = myObjects;
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Button btn = (Button)sender;
+            string idtobedeleted = (string)btn.CommandParameter;
+            List<MyObject> mDeletionList = new List<MyObject>();
+			List<MyObject> mEa = new List<MyObject>();
+		//	mDeletionList.Add((FreeToPlay.mViews.ListViewPage.MyObject)myObjects.Where(p => p.Name == idtobedeleted));
+            var mitem = myObjects.Where(p => p.Name == idtobedeleted);
+
+            //        foreach (MyObject item in myObjects)
+            //            {
+            //            if (item.Name == idtobedeleted)
+            //                {
+            //                    mEa.Add(item);
+            //                }
+            //            }
+            //        lstView.ItemsSource = null;
+            //        lstView.ItemsSource = myObjects;
+            //        foreach (var item in mEa)
+            //        {
+
+            //myObjects.Remove(item);
+            //}
+            //myObjects.Remove((FreeToPlay.mViews.ListViewPage.MyObject)myObjects.Where(p => p.Name == idtobedeleted));
+            foreach (MyObject item in mitem)
+            {
+                mEa.Add(item);               
+            }
+            foreach (var item in mEa)
+               {
+
+                myObjects.Remove(item);
+               }
+
+            lstView.ItemsSource = null;
             lstView.ItemsSource = myObjects;
         }
     }
